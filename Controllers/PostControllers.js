93ba5dd45298ca8d940fa4b42b1339748bcdd82e
Dominic Martin Cruz <dmcruz.wf@gmail.com>
2022-09-26 00:25:57 +0800

    added server

---
+++ Controllers/PostControllers.js
@@ -0,0 +1,126 @@
+import mongoose from 'mongoose';
+import PostModel from '../Models/postModel.js';
+import UserModel from '../Models/userModel.js';
+
+// Create a new Post
+export const createPost = async (req, res) => {
+  const newPost = new PostModel(req.body);
+
+  try {
+    await newPost.save();
+    res.status(200).json('Post created.');
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Get a Post
+export const getPost = async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json('Post does not exist.');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Update a Post
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('Post updated.');
+    } else {
+      res.status(403).json('Action forbidden.');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Delete a Post
+export const deletePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json('Post deleted.');
+    } else {
+      res.status(403).json('Action forbidden.');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Like/dislike a post
+export const likePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json('Post liked.');
+    } else {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json('Post unliked.');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Get Timeline Posts
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+    const followingPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: 'posts',
+          localField: 'following',
+          foreignField: 'userId',
+          as: 'followingPosts',
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return b.createdAt - a.createdAt;
+        })
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
---
+++ Models/postModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+    },
+    likes: [],
+    image: String,
+  },
+  { timestamps: true }
+);
+
+const PostModel = mongoose.model('Posts', PostSchema);
+export default PostModel;
